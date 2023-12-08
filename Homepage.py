cbb448a689d9b54e7ba1e10efec28f579a2320c9
Deussaxum <147807491+Deussaxum@users.noreply.github.com>
2023-12-08 21:21:40 +0100

    ss

--- Homepage.py
+++ Homepage.py
@@ -38,21 +38,45 @@ st.info("""
 
   
 
-image_url = "https://media.gq-magazin.de/photos/5f684a5c1744746f33a1c573/1:1/w_1248,h_1248,c_limit/leonardo-dicaprio-el-lobo-de-wall-street.jpg"
+common_image_url = "https://media.gq-magazin.de/photos/5f684a5c1744746f33a1c573/1:1/w_1248,h_1248,c_limit/leonardo-dicaprio-el-lobo-de-wall-street.jpg"
 
 col1, col2 = st.columns(2)
 
 # Box 1
 with col1:
     st.subheader("Consulting 🛫")
-    st.image(image_url)
+    st.image(common_image_url)
     st.write("As a consultant, you provide expert advice to organizations, helping them improve performance, operations, and profitability. Your role involves analyzing situations, identifying problems, and presenting comprehensive solutions to meet client needs.")
-st.write("   ")
-st.write("   ")
-st.write("   ")
-st.write("   ")
-st.write("   ")
-st.write("   ")
+
+# Box 2
+with col2:
+    st.subheader("Finance 📈")
+    st.image(common_image_url)
+    st.write("In Finance you offer financial advice, prepare lending agreements, and ensure accurate corporate records. Your role involves working with corporations of various sizes, providing services like credit, treasury, financing, and commercial analysis to meet their financial needs.")
+
+# Box 3
+with col1:
+    st.subheader("Corporate 🏢")
+    st.image(common_image_url)
+    st.write("In Corporate, you work within an organization with opportunities for career advancement, being responsible for a variety of roles including account management, providing financial advice, or ensuring accurate records to contribute to the company's business goals.")
+
+# Box 4
+with col2:
+    st.subheader("Start-Up 🚀")
+    st.image(common_image_url)
+    st.write("In a startup, you typically wear multiple hats, taking on various responsibilities that can range from strategic planning to hands-on execution. Your role may involve setting direction, creating culture, and driving growth, all while adapting to the fast-paced and ever-changing startup environment.")
+
+# Box 5
+with col1:
+    st.subheader("IT 💻")
+    st.image(common_image_url)
+    st.write("As an IT professional, you manage and store data using computers, software, databases, networks, and servers, and your role may include writing programs, maintaining networks, analyzing systems, and providing technical support.") 
+
+# Box 6
+with col2:
+    st.subheader("Academic 📚")
+    st.image(common_image_url)
+    st.write("Working in Academia involves teaching, guiding students through lectures and seminars, and conducts research to contribute to their field of expertise. You often participate in academic service such as serving on committees and reviewing scholarly work.")
 
 
 
