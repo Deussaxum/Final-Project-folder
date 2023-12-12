681972eadf444b33a8d163ad5e167840d4ce16cd
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-12 18:36:16 +0100

    j

--- pages/API People.py
+++ pages/API People.py
@@ -1,63 +1,85 @@
-import streamlit as st
 import requests
+import json 
 
-# Define your API key and headers
-api_key = '_EIqMpWEbOnJLoQvNFz1CQ'  # Be sure to replace with your actual API key
+api_key = '_EIqMpWEbOnJLoQvNFz1CQ'
 headers = {'Authorization': 'Bearer ' + api_key}
 api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+params = {
+    'linkedin_profile_url': 'https://www.linkedin.com/in/benjamin-david-auer/',
+    'use_cache': 'if-recent'  # Added the use_cache parameter
+}
+#comment next three lines when testing
+response = requests.get(api_endpoint,
+                       params=params,
+                       headers=headers)
 
-# Streamlit app layout
-st.set_page_config(layout="wide")  # Set the page layout to wide
-
-# Create columns for the header and the LinkedIn logo
-header_col, logo_col = st.columns([0.9, 0.1])
-
-# Use the first column to display the app title
-with header_col:
-    st.title('LinkedIn Profile Filler')
-
-# Use the second column to display the LinkedIn logo
-with logo_col:
-    linkedin_logo_url = 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
-    st.image(linkedin_logo_url, width=50)
-
-# Function to extract information from API response
 def extract_info(jsondata):
+
+    # Set default values to Not available
     extracted_info = {
-        'full_name': jsondata.get('full_name', 'Not available'),
-        'city': jsondata.get('city', 'Not available'),
-        'experiences': jsondata.get('experiences', [])
+        'full_name': 'Not available',
+        'city':'Not available',
+        'state': 'Not available',
+        'country': 'Not Available',
+        'education': [],
+        'occupation': 'Not Available',
+        'experiences': [],
+        'volunteer_work': 'Not Available',
+        'certifications': 'Not Available',
+        'languages': 'Not Available',
+        'interests': 'Not Available'
     }
-    return extracted_info
 
-# Function to retrieve information
-def retrieve_info():
-    params = {'linkedin_profile_url': linkedin_profile_url}
-    response = requests.get(api_endpoint, params=params, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        info = extract_info(data)
-        display_info(info)
-    else:
-        st.error(f"Failed to retrieve profile information: HTTP {response.status_code}")
-
-# Function to display profile information
-def display_info(info):
-    st.subheader('Profile Information:')
-    st.write('Full Name:', info['full_name'])
-    st.write('City:', info['city'])
+    # Replace values if found in response
+    if "full_name" in jsondata:
+        extracted_info["full_name"] = jsondata["full_name"]
     
-    st.subheader('Experiences:')
-    for exp in info['experiences']:
-        st.write('Title:', exp.get('title', 'Not available'))
-        st.write('Company:', exp.get('company', 'Not available'))
-        st.write('Description:', exp.get('description', 'Not available'))
-        st.write('---')  # Separator line
+    if "city" in jsondata:
+        extracted_info["city"]  = jsondata["city"]
 
-# Input field for LinkedIn profile URL
-linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL')
+    if "state" in jsondata:
+        extracted_info["state"]  = jsondata["state"]
 
-# Button to trigger the information retrieval
-if st.button('Retrieve Information'):
-    retrieve_info()
+    if "country" in jsondata:
+        extracted_info["country"]  = jsondata["country"]
 
+    if "education" in jsondata:
+        for educations in jsondata ["education"]:
+            extracted_info['education'].append({
+                'school': educations ["school"],
+                'degree_name': educations ["degree_name"],
+                'field_of_study': educations["field_of_study"],
+                'grade': educations["grade"]
+             })
+    if "occupation" in jsondata:
+        extracted_info["occupation"]  = jsondata["occupation"]
+
+    if "experiences" in jsondata:
+        for experience in jsondata["experiences"]:
+            extracted_info['experiences'].append({
+                'title': experience["title"],
+                'company': experience["company"],
+                'location': experience["location"]
+            })
+    if "volunteer_work" in jsondata:
+        extracted_info["volunteer_work"]  = jsondata["volunteer_work"]
+
+    if "certifications" in jsondata:
+        extracted_info["certifications"]  = jsondata["certifications"]
+
+    if "languages" in jsondata:
+        extracted_info["languages"]  = jsondata["languages"]
+
+    if "interests" in jsondata:
+        extracted_info["interests"]  = jsondata["interests"]
+
+    return extracted_info 
+# Check if the response was successful
+if response.status_code == 200:
+    jsondata = response.json()
+    # Call the extract_info function with the JSON data
+    extracted_info = extract_info(jsondata)
+    # Print the extracted information - Correct place to put the print function
+    print(extracted_info)
+else:
+    print("Error: Unable to fetch data. Status code:", response.status_code)
