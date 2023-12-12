fdf0f68621a3aaed64612f1c22933f66db07bd0f
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-12 18:40:38 +0100

    j

--- pages/API People.py
+++ pages/API People.py
@@ -1,85 +1,95 @@
+import streamlit as st
 import requests
-import json 
+import json
 
-api_key = '_EIqMpWEbOnJLoQvNFz1CQ'
-headers = {'Authorization': 'Bearer ' + api_key}
-api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
-params = {
-    'linkedin_profile_url': 'https://www.linkedin.com/in/benjamin-david-auer/',
-    'use_cache': 'if-recent'  # Added the use_cache parameter
-}
-#comment next three lines when testing
-response = requests.get(api_endpoint,
-                       params=params,
-                       headers=headers)
-
+# Function to extract info
 def extract_info(jsondata):
-
-    # Set default values to Not available
     extracted_info = {
         'full_name': 'Not available',
-        'city':'Not available',
+        'city': 'Not available',
         'state': 'Not available',
-        'country': 'Not Available',
+        'country': 'Not available',
         'education': [],
-        'occupation': 'Not Available',
+        'occupation': 'Not available',
         'experiences': [],
-        'volunteer_work': 'Not Available',
-        'certifications': 'Not Available',
-        'languages': 'Not Available',
-        'interests': 'Not Available'
+        'volunteer_work': 'Not available',
+        'certifications': 'Not available',
+        'languages': 'Not available',
+        'interests': 'Not available'
     }
 
-    # Replace values if found in response
     if "full_name" in jsondata:
         extracted_info["full_name"] = jsondata["full_name"]
-    
+
     if "city" in jsondata:
-        extracted_info["city"]  = jsondata["city"]
+        extracted_info["city"] = jsondata["city"]
 
     if "state" in jsondata:
-        extracted_info["state"]  = jsondata["state"]
+        extracted_info["state"] = jsondata["state"]
 
     if "country" in jsondata:
-        extracted_info["country"]  = jsondata["country"]
+        extracted_info["country"] = jsondata["country"]
 
     if "education" in jsondata:
-        for educations in jsondata ["education"]:
+        for educations in jsondata["education"]:
             extracted_info['education'].append({
-                'school': educations ["school"],
-                'degree_name': educations ["degree_name"],
-                'field_of_study': educations["field_of_study"],
-                'grade': educations["grade"]
-             })
+                'school': educations.get("school", "Not available"),
+                'degree_name': educations.get("degree_name", "Not available"),
+                'field_of_study': educations.get("field_of_study", "Not available"),
+                'grade': educations.get("grade", "Not available")
+            })
+
     if "occupation" in jsondata:
-        extracted_info["occupation"]  = jsondata["occupation"]
+        extracted_info["occupation"] = jsondata["occupation"]
 
     if "experiences" in jsondata:
         for experience in jsondata["experiences"]:
             extracted_info['experiences'].append({
-                'title': experience["title"],
-                'company': experience["company"],
-                'location': experience["location"]
+                'title': experience.get("title", "Not available"),
+                'company': experience.get("company", "Not available"),
+                'location': experience.get("location", "Not available")
             })
+
     if "volunteer_work" in jsondata:
-        extracted_info["volunteer_work"]  = jsondata["volunteer_work"]
+        extracted_info["volunteer_work"] = jsondata["volunteer_work"]
 
     if "certifications" in jsondata:
-        extracted_info["certifications"]  = jsondata["certifications"]
+        extracted_info["certifications"] = jsondata["certifications"]
 
     if "languages" in jsondata:
-        extracted_info["languages"]  = jsondata["languages"]
+        extracted_info["languages"] = jsondata["languages"]
 
     if "interests" in jsondata:
-        extracted_info["interests"]  = jsondata["interests"]
+        extracted_info["interests"] = jsondata["interests"]
 
-    return extracted_info 
-# Check if the response was successful
-if response.status_code == 200:
-    jsondata = response.json()
-    # Call the extract_info function with the JSON data
-    extracted_info = extract_info(jsondata)
-    # Print the extracted information - Correct place to put the print function
-    print(extracted_info)
-else:
-    print("Error: Unable to fetch data. Status code:", response.status_code)
+    return extracted_info
+
+# Streamlit application starts here
+st.title("LinkedIn Profile Scraper")
+
+# Input from user
+linkedin_url = st.text_input("Enter LinkedIn Profile URL", "")
+
+if st.button("Fetch Profile Data"):
+    if linkedin_url:
+        # Call your API with the URL
+        api_key = '_EIqMpWEbOnJLoQvNFz1CQ'
+        headers = {'Authorization': 'Bearer ' + api_key}
+        api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+        params = {
+            'linkedin_profile_url': linkedin_url,
+            'use_cache': 'if-recent'
+        }
+
+        response = requests.get(api_endpoint, params=params, headers=headers)
+
+        if response.status_code == 200:
+            jsondata = response.json()
+            extracted_info = extract_info(jsondata)
+
+            # Display the extracted information
+            st.write(extracted_info)
+        else:
+            st.error(f"Error: Unable to fetch data. Status code: {response.status_code}")
+    else:
+        st.error("Please enter a valid LinkedIn URL.")
