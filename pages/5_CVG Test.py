d910f007a051d8839c11de26d58f04f458af03ff
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-12 19:07:54 +0100

    j

--- pages/5_CVG Test.py
+++ pages/5_CVG Test.py
@@ -3,14 +3,18 @@ import requests
 
 # Function to extract information from API response
 def extract_info(jsondata):
+    # Initialize default values for all fields
     extracted_info = {
         'full_name': jsondata.get('full_name', ''),
         'city': jsondata.get('city', ''),
-        'experiences': jsondata.get('experiences', []),
+        'state': jsondata.get('state', ''),
+        'country': jsondata.get('country', ''),
         'education': jsondata.get('education', []),
+        'experiences': jsondata.get('experiences', []),
+        'volunteer_work': jsondata.get('volunteer_work', []),
+        'certifications': jsondata.get('certifications', []),
         'languages': jsondata.get('languages', []),
-        'interests': jsondata.get('interests', []),
-        'certifications': jsondata.get('certifications', [])
+        'interests': jsondata.get('interests', [])
     }
     return extracted_info
 
@@ -32,54 +36,53 @@ def retrieve_info(linkedin_profile_url):
 st.title("CV Generator 📃")
 linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', key='linkedin_url')
 
-tab_titles = ["Consulting 🧮", "Finance 📈", "Corporate 🏢", "Start-Up 🚀", "IT 🖥", "Academic 📚"]
-tabs = st.tabs(tab_titles)
+if st.button('Retrieve LinkedIn Data'):
+    linkedin_data = retrieve_info(linkedin_profile_url) or {}
 
-for tab, title in zip(tabs, tab_titles):
-    with tab:
-        linkedin_data = {}
-        if st.button(f'Get your input via LinkedIn for {title}', key=f'linkedin_button_{title}'):
-            linkedin_data = retrieve_info(linkedin_profile_url) or {}
+# Personal Information Section
+st.header("Personal Information")
+name = st.text_input("Name", value=linkedin_data.get('full_name', ''), key='name')
+address = st.text_input("Address", value=f"{linkedin_data.get('city', '')}, {linkedin_data.get('state', '')}, {linkedin_data.get('country', '')}", key='address')
+phone = st.text_input("Phone", key='phone')
+email = st.text_input("Email", key='email')
 
-        # Personal Information Section
-        st.header("Personal Information")
-        name = st.text_input("Name", value=linkedin_data.get('full_name', ''), key=f'name_{title}')
-        address = st.text_input("Address", value=linkedin_data.get('city', ''), key=f'address_{title}')
-        phone = st.text_input("Phone", key=f'phone_{title}')
-        email = st.text_input("Email", key=f'email_{title}')
+# Education Section
+st.header("Education")
+for i in range(2):  # Assuming up to 2 education entries
+    with st.expander(f"Education {i+1}"):
+        education = linkedin_data.get('education', [{}])[i] if i < len(linkedin_data.get('education', [])) else {}
+        university = st.text_input(f"University/School {i+1}", value=education.get('school', ''), key=f'university_{i}')
+        degree = st.text_input(f"Degree {i+1}", value=education.get('degree_name', ''), key=f'degree_{i}')
+        field_of_study = st.text_input(f"Field of Study {i+1}", value=education.get('field_of_study', ''), key=f'field_of_study_{i}')
+        gpa = st.text_input(f"GPA {i+1}", value=education.get('grade', ''), key=f'gpa_{i}')
 
-        # Education Section
-        st.header("Education")
-        for i in range(2):  # Assuming up to 2 education entries
-            with st.expander(f"Education {i+1}"):
-                university = st.text_input(f"University/School {i+1}", value=linkedin_data.get('education', [{}])[i].get('school', ''), key=f'university_{i}_{title}')
-                degree = st.text_input(f"Degree {i+1}", value=linkedin_data.get('education', [{}])[i].get('degree_name', ''), key=f'degree_{i}_{title}')
-                field_of_study = st.text_input(f"Field of Study {i+1}", value=linkedin_data.get('education', [{}])[i].get('field_of_study', ''), key=f'field_of_study_{i}_{title}')
-                gpa = st.text_input(f"GPA {i+1}", value=linkedin_data.get('education', [{}])[i].get('grade', ''), key=f'gpa_{i}_{title}')
+# Professional Experience Section
+st.header("Professional Experience")
+for j in range(3):  # Assuming up to 3 experiences
+    with st.expander(f"Experience {j+1}"):
+        experience = linkedin_data.get('experiences', [{}])[j] if j < len(linkedin_data.get('experiences', [])) else {}
+        company = st.text_input(f"Company {j+1}", value=experience.get('company', ''), key=f'company_{j}')
+        position = st.text_input(f"Position {j+1}", value=experience.get('title', ''), key=f'position_{j}')
+        location = st.text_input(f"Location {j+1}", value=experience.get('location', ''), key=f'location_{j}')
+        description = st.text_area(f"Description {j+1}", value=experience.get('description', ''), key=f'description_{j}', height=150)
 
-        # Professional Experience Section
-        st.header("Professional Experience")
-        for j in range(3):  # Assuming up to 3 experiences
-            with st.expander(f"Experience {j+1}"):
-                exp = linkedin_data.get('experiences', [{}])[j]
-                company = st.text_input(f"Company {j+1}", value=exp.get('company', ''), key=f'company_{j}_{title}')
-                position = st.text_input(f"Position {j+1}", value=exp.get('title', ''), key=f'position_{j}_{title}')
-                location = st.text_input(f"Location {j+1}", value=exp.get('location', ''), key=f'location_{j}_{title}')
-                # No field for duration in the API data
+# Extracurricular Activities Section
+st.header("Extracurricular Activities / Engagement")
+volunteer_works = ', '.join([work.get('title', '') for work in linkedin_data.get('volunteer_work', [])])
+activities = st.text_input("Extracurricular Activities", value=volunteer_works, key='activities')
 
-        # Extracurricular Activities Section
-        st.header("Extracurricular Activities / Engagement")
-        activities = st.text_input("Extracurricular Activities", value=', '.join([act.get('title', '') for act in linkedin_data.get('volunteer_work', [])]), key=f'activities_{title}')
-        additional_education = st.text_input("Additional Education", key=f'additional_education_{title}')
-        certificates = st.text_input("Certificates and Achievements", value=', '.join([cert.get('name', '') for cert in linkedin_data.get('certifications', [])]), key=f'certificates_{title}')
+# Additional Education Section
+st.header("Additional Education")
+certifications = ', '.join([cert.get('name', '') for cert in linkedin_data.get('certifications', [])])
+additional_education = st.text_input("Certificates and Achievements", value=certifications, key='additional_education')
 
-        # Skills & Interest Section
-        st.header("Skills & Interest")
-        languages = st.text_input("Languages", value=', '.join(linkedin_data.get('languages', [])), key=f'languages_{title}')
-        computer_skills = st.text_input("Computer Skills", key=f'computer_skills_{title}')
-        interests = st.text_input("Interests", value=', '.join(linkedin_data.get('interests', [])), key=f'interests_{title}')
+# Skills & Interest Section
+st.header("Skills & Interest")
+languages = st.text_input("Languages", value=', '.join(linkedin_data.get('languages', [])), key='languages')
+computer_skills = st.text_input("Computer Skills", key='computer_skills')
+interests = st.text_input("Interests", value=', '.join(linkedin_data.get('interests', [])), key='interests')
 
-        # Button to create the CV
-        if st.button(f"Create CV for {title}", key=f'create_cv_{title}'):
-            # Logic to create and display/download the CV
-            pass
+# Button to create the CV
+if st.button("Create CV"):
+    # Logic to create and display/download the CV
+    pass
