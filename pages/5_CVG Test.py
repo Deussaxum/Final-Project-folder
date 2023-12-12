9de55e5d1410e5246fff0648c60fc334bdd9a28f
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-12 21:06:26 +0100

    j

--- pages/5_CVG Test.py
+++ pages/5_CVG Test.py
@@ -37,65 +37,62 @@ linkedin_data = {}
 
 # Streamlit app layout
 st.title("CV Generator 📃")
-linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', key='linkedin_url')
+linkedin_profile_url = st.text_input('Enter your LinkedIn profile URL', key='linkedin_url_key')
 
-if st.button('Retrieve LinkedIn Data'):
+if st.button('Retrieve LinkedIn Data', key='retrieve_data_button'):
     linkedin_data = retrieve_info(linkedin_profile_url) or {}
 
 # Personal Information Section
 st.header("Persönliche Informationen")
-name = st.text_input("Name", value=linkedin_data.get('full_name', ''), key='unique_key_1')
-address = st.text_input("Adresse", value=f"{linkedin_data.get('city', '')}, {linkedin_data.get('state', '')}, {linkedin_data.get('country', '')}", key='unique_key_2')
-phone = st.text_input("Telefonnummer", key='unique_key_3')
-email = st.text_input("E-Mail", key='unique_key_4')
+name = st.text_input("Name", value=linkedin_data.get('full_name', ''), key='name_key')
+address = st.text_input("Adresse", value=f"{linkedin_data.get('city', '')}, {linkedin_data.get('state', '')}, {linkedin_data.get('country', '')}", key='address_key')
+phone = st.text_input("Telefonnummer", key='phone_key')
+email = st.text_input("E-Mail", key='email_key')
 
 # Education Section
 st.header("Education")
 # Assuming the first two education entries in LinkedIn data (if they exist) are to be used
 education_entries = linkedin_data.get('education', [{} for _ in range(2)])
-for i, education in enumerate(education_entries[:2]):
+for i in range(2):
     with st.expander(f"Education {i+1}"):
-        university_key = f'unique_key_{5 + i*7}'
-        majorus_key = f'unique_key_{7 + i*7}'
-        university = st.text_input(f"Universität/Schule {i+1}", value=education.get('school', ''), key=university_key)
-        locationus = st.text_input(f"Standort {i+1}", value=education.get('location', ''), key=f'unique_key_{6 + i*7}')
-        majorus = st.text_input(f"Studiengang {i+1}", value=education.get('field_of_study', ''), key=majorus_key)
-        timeus = st.text_input(f"Zeitraum {i+1}", key=f'unique_key_{8 + i*7}')  # No LinkedIn data for the time period
-        courses = st.text_input(f"Kurse {i+1}", key=f'unique_key_{9 + i*7}')  # No LinkedIn data for courses
-        gpa = st.text_input(f"GPA {i+1}", value=education.get('grade', ''), key=f'unique_key_{10 + i*7}')
-        clubs = st.text_input(f"Clubs/Aktivitäten {i+1}", key=f'unique_key_{11 + i*7}')  # No LinkedIn data for clubs/activities
+        education = education_entries[i]
+        university = st.text_input(f"Universität/Schule {i+1}", value=education.get('school', ''), key=f'university_{i+1}_key')
+        locationus = st.text_input(f"Standort {i+1}", value=education.get('location', ''), key=f'location_{i+1}_key')
+        majorus = st.text_input(f"Studiengang {i+1}", value=education.get('field_of_study', ''), key=f'major_{i+1}_key')
+        timeus = st.text_input(f"Zeitraum {i+1}", key=f'time_{i+1}_key')  # No LinkedIn data for the time period
+        courses = st.text_input(f"Kurse {i+1}", key=f'courses_{i+1}_key')  # No LinkedIn data for courses
+        gpa = st.text_input(f"GPA {i+1}", value=education.get('grade', ''), key=f'gpa_{i+1}_key')
+        clubs = st.text_input(f"Clubs/Aktivitäten {i+1}", key=f'clubs_{i+1}_key')  # No LinkedIn data for clubs/activities
 
 # Professional Experience Section
 st.header("Professional Experience")
 # Assuming the first three experience entries in LinkedIn data (if they exist) are to be used
 experience_entries = linkedin_data.get('experiences', [{} for _ in range(3)])
-for j, experience in enumerate(experience_entries[:3]):
+for j in range(3):
     with st.expander(f"Experience {j+1}"):
-        experience_key = f'unique_key_{19 + j*7}'
-        locatione_key = f'unique_key_{20 + j*7}'
-        position_key = f'unique_key_{21 + j*7}'
-        company = st.text_input(f"Erfahrung {j+1}", value=experience.get('company', ''), key=experience_key)
-        locatione = st.text_input(f"Standort Erfahrung {j+1}", value=experience.get('location', ''), key=locatione_key)
-        position = st.text_input(f"Position {j+1}", value=experience.get('title', ''), key=position_key)
-        timee = st.text_input(f"Zeitraum Erfahrung {j+1}", key=f'unique_key_{22 + j*7}')  # No LinkedIn data for the time period
+        experience = experience_entries[j]
+        company = st.text_input(f"Erfahrung {j+1}", value=experience.get('company', ''), key=f'company_{j+1}_key')
+        locatione = st.text_input(f"Standort Erfahrung {j+1}", value=experience.get('location', ''), key=f'experience_location_{j+1}_key')
+        position = st.text_input(f"Position {j+1}", value=experience.get('title', ''), key=f'position_{j+1}_key')
+        timee = st.text_input(f"Zeitraum Erfahrung {j+1}", key=f'experience_time_{j+1}_key')
         # No LinkedIn data for tasks, so leaving them blank for user input
-        task1 = st.text_area(f"Aufgaben 1", key=f'task{1 + j}_1', height=100)
-        task2 = st.text_area(f"Aufgaben 2", key=f'task{1 + j}_2', height=100)
-        task3 = st.text_area(f"Aufgaben 3", key=f'task{1 + j}_3', height=100)
+        task1 = st.text_area(f"Aufgaben {j+1} - 1", key=f'task_{j+1}_1_key', height=100)
+        task2 = st.text_area(f"Aufgaben {j+1} - 2", key=f'task_{j+1}_2_key', height=100)
+        task3 = st.text_area(f"Aufgaben {j+1} - 3", key=f'task_{j+1}_3_key', height=100)
 
 # Extracurricular Activities / Engagement Section
 st.header("Extracurricular Activities / Engagement")
-extracurricular1 = st.text_input("Extrakurrikulare Aktivitäten", key="unique_key_30")
-additionaleducation1 = st.text_input("Zusätzliche Bildung", key="unique_key_31")
-certificates1 = st.text_input("Zertifikate und Errungenschaften", key="unique_key_32")
+extracurricular1 = st.text_input("Extrakurrikulare Aktivitäten", key="extracurricular_1_key")
+additionaleducation1 = st.text_input("Zusätzliche Bildung", key="additional_education_1_key")
+certificates1 = st.text_input("Zertifikate und Errungenschaften", key="certificates_1_key")
 
 # Skills & Interest Section
 st.header("Skills & Interest")
-languages1 = st.text_input("Sprachen", key="unique_key_33")
-computer1 = st.text_input("Computerkenntnisse", key="unique_key_34")
-interests1 = st.text_input("Interessen", key="unique_key_35")
+languages1 = st.text_input("Sprachen", key="languages_1_key")
+computer1 = st.text_input("Computerkenntnisse", key="computer_skills_key")
+interests1 = st.text_input("Interessen", key="interests_1_key")
 
 # Button to create the CV
-if st.button("Create CV"):
+if st.button("Create CV", key='create_cv_button'):
     # Logic to create and display/download the CV
     pass
