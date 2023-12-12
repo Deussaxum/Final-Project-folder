bfe30f71730f90f163ccdf4583f66da7cafbee07
tristanehlers <148540430+tristanehlers@users.noreply.github.com>
2023-12-12 23:02:59 +0100

    j

--- pages/5_CVG Test.py
+++ pages/5_CVG Test.py
@@ -211,8 +211,72 @@ with st.expander("Skills & Interest", expanded=False):  # 'expanded=True' means 
     computer1 = st.text_input("Computerkenntnisse", value=computer_skills_combined, key="computer_skills_key")
     interests1 = st.text_input("Interessen", value=interests_combined, key="interests_1_key")
 
-# Button to create the CV
-if st.button("Create CV", key='create_cv_button'):
-    # Logic to create and display/download the CV
-    pass
+# Button zum Erstellen des CVs
+if st.button("CV Erstellen", key="unique_key_143"):
+    try:
+        with open('template_finance.tex', 'r', encoding='utf-8') as file:
+            latex_template = file.read()
+
+        try:
+            # Formatierung des LaTeX-Templates
+            latex_filled = latex_template.format(
+                name=name,
+                address=address,
+                phone=phone,
+                email=email,
+                university1=university1, 
+                locationus1=locationus1, 
+                majorus1=majorus1, 
+                timeus1=timeus1,
+                courses1=courses1, 
+                gpa1=gpa1, 
+                clubs1=clubs1,
+                university2=university2, 
+                locationus2=locationus2, 
+                majorus2=majorus2, 
+                timeus2=timeus2, 
+                courses2=courses2, 
+                gpa2=gpa2, 
+                clubs2=clubs2, 
+                experience1=experience1, 
+                locatione1=locatione1, 
+                position1=position1, 
+                timee1=timee1, 
+                task11=task11, 
+                task12=task12, 
+                task13=task13, 
+                experience2=experience2, 
+                locatione2=locatione2, 
+                position2=position2, 
+                timee2=timee2, 
+                task21=task21, 
+                task22=task22, 
+                task23=task23, 
+                experience3=experience3,
+                locatione3=locatione3, 
+                position3=position3, 
+                timee3=timee3, 
+                task31=task31, 
+                task32=task32, 
+                task33=task33, 
+                extracurricular1=extracurricular1, 
+                additionaleducation1=additionaleducation1, 
+                certificates1=certificates1, 
+                languages1=languages1,
+                computer1=computer1, 
+                interests1=interests1
+            )
+
+            # Anzeigen des gefüllten LaTeX-Codes auf der Streamlit-Oberfläche
+            st.text_area("Gefüllter LaTeX-Code", latex_filled, height=300)
+
+        except KeyError as key_err:
+            st.error(f"Fehler bei der Formatierung: Unbekannter Platzhalter {key_err}")
+        except Exception as format_err:
+            st.error(f"Fehler bei der Formatierung: {format_err}")
+
+    except FileNotFoundError:
+        st.error("Die LaTeX-Vorlagendatei wurde nicht gefunden.")
+    except Exception as e:
+        st.error(f"Ein unerwarteter Fehler ist aufgetreten: {e}")
 
